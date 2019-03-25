494b1cf5bb48b626b1767e3423057ec59ee879c0
Aritra Kumar Lahiri <lahiri.aritra@gmail.com>
2019-03-24 23:09:44 -0700

    Add bubble and pie plot in plotly

--- SourceCode/Belly_Button_Biodiversity/static/js/app.js
+++ SourceCode/Belly_Button_Biodiversity/static/js/app.js
@@ -3,13 +3,19 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
+  var metaDataUrl = "/metadata/" + sample;
     // Use d3 to select the panel with id of `#sample-metadata`
-
+  var panelMetadata = d3.select("#sample-metadata");  
     // Use `.html("") to clear any existing metadata
-
+  panelMetadata.html("");  
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+    d3.json(metaDataUrl).then(function (data) {
+      Object.entries(data).forEach(([key, value]) => {
+        panelMetadata.append("h6").text(`${key}: ${value}`);
+      });
+    }); 
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
@@ -18,12 +24,45 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-
+  var chartsURL = "/samples/" + sample;
+  d3.json(chartsURL).then(function (data) {
     // @TODO: Build a Bubble Chart using the sample data
+    var trace1 = {
+      x: data.otu_ids,
+      y: data.sample_values,
+      mode: 'markers',
+      text: data.otu_labels,
+      marker: {
+        color: data.otu_ids,
+        size: data.sample_values, 
+        colorscale: "Earth"
+      }
+    };
+    var traceData1 = [trace1];
+    var layout1 = {
+      showlegend: false,
+      height: 600,
+      width: 1500
+    };
+    
+    Plotly.newPlot('bubble', traceData1, layout1);  
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+    var trace2 = [{
+      values: data.sample_values.slice(0, 10),
+      labels: data.otu_ids.slice(0, 10),
+      hovertext: data.otu_labels.slice(0, 10),
+      type: 'pie',
+    }];
+    var layout2 = {
+      showlegend: true,
+    };
+    var traceData2 = [trace2];
+    Plotly.newPlot('pie', traceData2, layout2);
+
+  });
 }
 
 function init() {
